ae17826635cce1e93c2472a0834eb22fdaf63e93
Valdoveste <nickyv.oliveira@gmail.com>
2020-07-24 03:13:00 -0300

    code for popup included

--- src/js/fileds.js
+++ src/js/fileds.js
@@ -1,8 +1,11 @@
 /* #4527a0 */
-const fields = document.querySelectorAll('input[data-js="number"]');
+const fieldCPF = document.getElementsByTagName("input")[1];
+const fields = document.querySelectorAll('input[data-js="algorism"]');
 const btnReset = document.getElementsByTagName("button")[0];
+const popUP = document.getElementById("info-cpf-popup");
 const RegEx = [
-/(^[A-ZÀ-Ú-a-zà-ú_@/#&+*]*$)/,
+/([A-ZÀ-Ú-a-zà-ú_@/#&+*.])/,
+/([A-ZÀ-Úa-zà-ú_@/#&+*.])/,
 /([(1){6}]{6})|([(2){6}]{6})|([(3){6}]{6})|([(4){6}]{6})|([(5){6}]{6})|([(6){6}]{6})|([(7){6}]{6})|([(8){6}]{6})|([(9){6}]{6})|([(0){6}]{6})/g
 ];
 
@@ -18,17 +21,16 @@ btnReset.addEventListener("mouseout", eventAnimation => {
     btnReset.style.transform = "rotate(360deg)";
 });
 
-fields[2].addEventListener("change", eventformatingPhone => {
-    fields[2].value = fields[2].value.replace(/(\d{5})/, '$1-');
+fields[1].addEventListener("change", eventformatingPhone => {
+    fields[1].value = fields[1].value.replace(/(\d{5})/, '$1-');
 });
 
-fields[4].addEventListener("change", eventformatingZIPCODE => {
-    fields[4].value = fields[4].value.replace(/(\d{5})/, '$1-');
+fields[3].addEventListener("change", eventformatingZIPCODE => {
+    fields[3].value = fields[3].value.replace(/(\d{5})/, '$1-');
 });
 
 
 /* 
-Panda
 Select all fields with "data-js = number", and add
 a "RegeEx" and "Listener", which prevents the user from typing
 alphabetic letters and some special characters.
@@ -36,25 +38,69 @@ alphabetic letters and some special characters.
 for(let item of fields){
     item.addEventListener("input", eventcleanCharac => {
         for(item of fields){
-        item.value = item.value.replace(RegEx[0], '');
+            item.value = item.value.replace(RegEx[1], '');
         }
     });
 }
 
-/* 123 456 789 00 */
-fields[0].addEventListener("change", eventgetCPF => {
-    fields[0].value = fields[0].value.replace(/(\d{3})(\d{3})(\d{3})(\d{1,2})/, '$1.$2.$3-$4');
-    const cpf = fields[0].value.replace(/[^0-9]/g, ''); // Clear the special characters added above.
-    if(cpf.match(RegEx[1]) || cpf.length < 11){
-        fields[0].value = fields[0].value.replace(/(.)/g, ''); //Clean the CPF input.
-        fields[0].style.boxShadow = "0px 0px 1.5px 1px rgb(255, 0, 0)"; //Simulates the CPF input in an invalid state.
-    }else{
-        if(validatesCPF(cpf)){ 
-            fields[0].style.boxShadow = "0px 0px 1.5px 1px rgb(0, 221, 0)"; //Confirms the validity of the CPF.
-        }
+fieldCPF.addEventListener("input", eventcleanCharc => {
+    fieldCPF.value = cleanCharacter(fieldCPF.value);
+
+    function cleanCharacter(x){
+        return x.replace(RegEx[0], '');
     }
 });
 
+/* 123 456 789 00 */
+fieldCPF.addEventListener("change", eventgetCPF => {
+    fieldCPF.value = fieldCPF.value.replace(/(\d{3})(\d{3})(\d{3})(\d{1,2})/, '$1.$2.$3-$4');
+
+    const cpf = fieldCPF.value.replace(/[^0-9]/g, ''); // Clear the special characters added above.
+
+    if(cpf.match(RegEx[2]) || cpf.length < 11){
+        invalidCPF();
+    }else{
+        if(validatesCPF(cpf)){ 
+            fieldCPF.style.boxShadow = "0px 0px 1.5px 1px rgb(0, 221, 0)"; //Confirms the validity of the CPF.
+            popUP.style.boxShadow = "0px 0px 1.5px 1px rgb(0, 221, 0)";
+            popUP.innerHTML = "Your CPF is valid!";
+            fieldCPF.title = "Your CPF is valid!";
+            popUP.style.display = "block";
+        }else{
+            invalidCPF();
+        }
+    }
+    
+    function invalidCPF(){
+        popUP.style.display = "block";
+        popUP.innerHTML = "Your CPF is invalid!";
+        popUP.style.boxShadow = "0px 0px 1.5px 1px rgb(255, 0, 0)";
+        fieldCPF.style.boxShadow = "0px 0px 1.5px 1px rgb(255, 0, 0)"; //Simulates the CPF input in an invalid state.
+        fieldCPF.title = "Your CPF is invalid, please click on the right popup to clean this field.";
+        fieldCPF.focus();
+    }
+});
+
+popUP.addEventListener("click", eventclosePopup => {
+    if(popUP.innerHTML === "Your CPF is invalid!"){
+        fieldCPF.value = fieldCPF.value.replace(/(.)/g, ''); //Clean the CPF input.
+        popUP.style.display = "none";
+    }
+});
+
+/* 
+    Whais CPF? 
+
+    The Individual Taxpayer Registry is the register 
+    maintained by the Federal Revenue of Brazil in which
+    any natural persons, regardless of age or nationality,
+    including those who have died, can register once. Each
+    registrant is uniquely identified by an 11 decimal 
+    digitCPF registration number.
+
+    So basically this part verify if "CPF" is valid.
+
+*/
 function validatesCPF(x) {
 let sum = 0;
 let digValidator = "";
